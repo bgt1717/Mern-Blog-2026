5a6e28d3cee65bc232d457bd1d040c8ebb25d35e
bgt1717 <brycetown10@gmail.com>
2026-02-10 20:36:49 -0600

    Edit Post fix

--- backend/routes/Post.js
+++ backend/routes/Post.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { createPost, getPosts } from "../controllers/postController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import { updatePost } from "../controllers/postController.js";
+import {deletePost} from "../controllers/postController.js";
 
 const router = express.Router();
 
@@ -8,6 +10,7 @@ router.get("/", getPosts);      // Public
 router.post("/", protect, createPost);  // Protected
 router.put("/:id", protect, updatePost);
 router.delete("/:id", protect, deletePost);
+router.put("/:id", protect, updatePost);
 
 
 export default router;
