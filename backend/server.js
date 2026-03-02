289fb1b49260b2b6ba9dedad680bc1c09e7fcf6b
bgt1717 <brycetown10@gmail.com>
2026-03-01 19:20:49 -0600

    cors updates

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://townsendblog.onrender.com",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes); // ✅ NO ()
