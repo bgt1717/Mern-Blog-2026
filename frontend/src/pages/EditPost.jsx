cd243a6651afa09bd890acb013e49365468bf0e5
bgt1717 <brycetown10@gmail.com>
2026-04-04 23:40:42 -0500

    EditPage update

--- frontend/src/pages/EditPost.jsx
+++ frontend/src/pages/EditPost.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import API from "../api/axios";
+import "./EditPost.css";
 
 export default function EditPost() {
   const { id } = useParams();
