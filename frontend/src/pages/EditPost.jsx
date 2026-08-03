7ee61fc9cb9415e1d3cb139ab659e2022d584137
bgt1717 <brycetown10@gmail.com>
2026-08-02 20:31:07 -0500

    deployment fix

--- frontend/src/pages/EditPost.jsx
+++ frontend/src/pages/EditPost.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import API from "../api/axios";
-import RichTextEditor from "./RichTextEditor";
+import RichTextEditor from "./richTextEditor";
 
 import "./EditPost.css";
 
